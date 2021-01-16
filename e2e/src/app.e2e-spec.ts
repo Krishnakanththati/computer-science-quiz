c77b59f54c627445a3a853b13dc9172b8e9140a0
Krishna Kanth <krishna@flujo.io>
2021-01-16 10:42:17 +0530

    commit

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('quiz-app app is running!');
+    expect(page.getTitleText()).toEqual('computer-science-quiz app is running!');
   });
 
   afterEach(async () => {
